745dd6775a2c63a488387be6e75de81d484229fb
Dgibbins <nayr_ryan@hotmail.com>
2017-02-06 17:59:56 -0500

    Flash! alerts + notices added

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
+  get 'sessions/create'
+
+  get 'sessions/destroy'
+
   # get 'users/new'
   # get 'users/create'
   # get 'products/index'
@@ -7,6 +13,7 @@ Rails.application.routes.draw do
   # get 'products/edit'
   resources :products
   resource  :users, only: [:new, :create]
+  resource  :sessions, only: [:new, :create, :destroy]
   root 'products#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

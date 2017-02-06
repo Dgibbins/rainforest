8f29f8f4a6c0ceff9ed4969c6e4b4591e51ab820
Dgibbins <nayr_ryan@hotmail.com>
2017-02-06 17:50:26 -0500

    Creating a user model, controller and view

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,13 @@
 class Product < ApplicationRecord
+
   validates :description, :name, presence:{ message: "must be given please" }
   validates :price_in_cents, numericality: {only_integer: true}
+
+  def dollars_conversion
+    price_in_dollars = price_in_cents.to_f / 100
+    sprintf"%.2f", price_in_dollars
+
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
+  # get 'users/new'
+  # get 'users/create'
   # get 'products/index'
   # get 'products/show'
   # get 'products/new'
   # get 'products/edit'
   resources :products
+  resource  :users, only: [:new, :create]
   root 'products#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
